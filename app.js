64fb5943fdc8756199d7e13a365795d93a59ee46
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-09 02:22:05 +0530

    updates

---
+++ app.js
@@ -0,0 +1,7 @@
+var haveEvents = 'GamepadEvent' in window;
+var haveWebkitEvents = 'WebKitGamepadEvent' in window;
+var controllers = {};
+var rAF = window.mozRequestAnimationFrame || 
+        window.webkitRequestAnimationFrame ||
+        window.requestAnimationFrame;
+
